5255c455ca761587284e79226ff446206d77a66b
Daemonium97 <daemonium97@gmail.com>
2021-03-09 18:38:52 +0000

    Creando gets en flux para consumir la API de GHIBLI

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,47 +1,35 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+            //se almacena la data que viene de la API
+            films: [],
+            characteres: [],
+            locations: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: () => {
-				// fetching data from the backend
-				fetch(process.env.BACKEND_URL + "/api/hello")
-					.then(resp => resp.json())
-					.then(data => setStore({ message: data.message }))
-					.catch(error => console.log("Error loading message from backend", error));
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+                //Get de la data Films
+            getFilms: async () => {
+                const url = "https://ghibliapi.herokuapp.com/films";
+                const response = await fetch(url);
+                const data = await response.json();
+                setStore({films: data})
+            },
+            //Get de la data people
+            getCha: async () => {
+                const url = "https://ghibliapi.herokuapp.com/people";
+                const response = await fetch(url);
+                const data = await response.json();
+                setStore({characteres: data})
+            },
+            //Get de la data locations
+            getLocations: async () => {
+                const url = "https://ghibliapi.herokuapp.com/locations";
+                const response = await fetch(url);
+                const data = await response.json();
+                setStore({locations: data})
+            }
+        
+            
 		}
 	};
 };
