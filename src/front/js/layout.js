4059c31b441792a8a8334e6a33e4c18c49da8105
Juan Diego Coto <jd506@hotmail.com>
2021-03-10 22:19:49 +0000

    Modifying Layout routes

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -18,68 +18,67 @@ import { AnimatePresence } from "framer-motion";
 
 //create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
-    const { store, actions } = useContext(Context);
-    useEffect(() => {
-        actions.getFilms();
-        actions.getCha();
-        actions.getLocations();
-    }, []);
+	//the basename is used when your project is published in a subdirectory and not in the root of the domain
+	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+	const basename = process.env.BASENAME || "";
+	const { store, actions } = useContext(Context);
+	useEffect(() => {
+		actions.getFilms();
+		actions.getCha();
+		actions.getLocations();
+	}, []);
 
-    return (
-        <div className="d-flex flex-column h-100">
-            <BrowserRouter basename={basename}>
-                <AnimatePresence>
-                    <Switch>
-                        <Route exact path="/">
-                            <Menu />
-                            <Logo />
-                            <Home />
-                            <Characters />
-                        </Route>
+	return (
+		<div className="d-flex flex-column h-100">
+			<BrowserRouter basename={basename}>
+				<AnimatePresence>
+					<Switch>
+						<Route exact path="/">
+							<Menu />
+							<Logo />
+							<Home />
+							<Characters />
+						</Route>
 
-                        <Route exact path="/places/:theid">
-                            <Menu />
-						    <Places data={store.locations}/>
-                        </Route>
+						<Route exact path="/places/:theid">
+							<Menu />
+							<Places data={store.locations} />
+						</Route>
 
-                        <Route exact path="/characters/:theid">
-                            <Menu />
-                            <Characters data={store.characteres} />
-                        </Route>
+						<Route exact path="/characters/:theid">
+							<Menu />
+							<Characters data={store.characteres} />
+						</Route>
 
-                        <Route exact path="/films/home">
-                            <Menu />
-                            <Films data={store.films} />
-                        </Route>
-                
+						<Route exact path="/films/home">
+							<Menu />
+							<Films data={store.films} />
+						</Route>
 
-                        <Route exact path="/descriptionCharacters/:theid">
-                            <Menu />
-                            <DescriptionCharacters />
-                        </Route>
+						<Route exact path="/descriptionCharacters/:theid">
+							<Menu />
+							<DescriptionCharacters />
+						</Route>
 
-                        <Route exact path="/descriptionPlaces/:theid">
-                            <Menu />
-                            <DescriptionPlaces />
-                        </Route> 
+						<Route exact path="/descriptionPlaces/:theid">
+							<Menu />
+							<DescriptionPlaces />
+						</Route>
 
-                        <Route exact path="/descriptionFilms/:theid">
-                            <Menu />
-                            <DescriptionFilms />
-                        </Route>
+						<Route exact path="/descriptionFilms/:theid">
+							<Menu />
+							<DescriptionFilms />
+						</Route>
 
-                        <Route>
-                            <h1>Not found!</h1>
-                        </Route>
-                    </Switch>
-                </AnimatePresence>
-                <Footer />
-            </BrowserRouter>
-        </div>
-    );
+						<Route>
+							<h1>Not found!</h1>
+						</Route>
+					</Switch>
+				</AnimatePresence>
+				<Footer />
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default injectContext(Layout);
