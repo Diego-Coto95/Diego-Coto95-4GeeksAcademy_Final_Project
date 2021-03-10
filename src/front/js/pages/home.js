337e9b021c5c3c7f4964c174fddd696beaef50cd
Daemonium97 <daemonium97@gmail.com>
2021-03-10 16:42:10 +0000

    Creating animations

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,4 +1,5 @@
 import React, { useState, useCallback, useContext } from "react";
+import { Link } from "react-router-dom";
 import Carousel from "react-bootstrap/Carousel";
 import { Context } from "../store/appContext";
 import "../../styles/home.scss";
@@ -12,39 +13,47 @@ export const Logo = () => {
 
 export const Home = () => {
 	return (
-        <motion.div>
+		<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+			<Carousel>
+				<Carousel.Item>
+					<Link to="/films">
+						<img
+							className="d-block w-100"
+							src="https://wallpapercave.com/wp/wp4603979.jpg"
+							alt="First slide"
+						/>
+					</Link>
+					<Carousel.Caption>
+						<h3 style={{ color: "black" }}>My Neighbor Totoro</h3>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<Link to="">
+						<img
+							className="d-block w-100"
+							src="https://images3.alphacoders.com/245/thumb-1920-245067.jpg"
+							alt="Second slide"
+						/>
+					</Link>
 
-        
-		<Carousel>
-			<Carousel.Item>
-				<img className="d-block w-100" src="https://wallpapercave.com/wp/wp4603979.jpg" alt="First slide" />
-				<Carousel.Caption>
-					<h3 style={{ color: "black" }}>My Neighbor Totoro</h3>
-				</Carousel.Caption>
-			</Carousel.Item>
-			<Carousel.Item>
-				<img
-					className="d-block w-100"
-					src="https://images3.alphacoders.com/245/thumb-1920-245067.jpg"
-					alt="Second slide"
-				/>
+					<Carousel.Caption>
+						<h3 style={{ color: "white" }}>{nameMovie}</h3>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<Link>
+						<img
+							className="d-block w-100"
+							src="https://www.whats-on-netflix.com/wp-content/uploads/2020/03/beginners-guide-to-studio-ghibli-movies-on-netflix.jpg"
+							alt="Third slide"
+						/>
+					</Link>
 
-				<Carousel.Caption>
-					<h3 style={{ color: "white" }}>{nameMovie}</h3>
-				</Carousel.Caption>
-			</Carousel.Item>
-			<Carousel.Item>
-				<img
-					className="d-block w-100"
-					src="https://www.whats-on-netflix.com/wp-content/uploads/2020/03/beginners-guide-to-studio-ghibli-movies-on-netflix.jpg"
-					alt="Third slide"
-				/>
-
-				<Carousel.Caption>
-					<h3 style={{ color: "black" }}>Here you can see all the movies.</h3>
-				</Carousel.Caption>
-			</Carousel.Item>
-		</Carousel>
-        </motion.div>
+					<Carousel.Caption>
+						<h3 style={{ color: "black" }}>Here you can see all the movies.</h3>
+					</Carousel.Caption>
+				</Carousel.Item>
+			</Carousel>
+		</motion.div>
 	);
 };
