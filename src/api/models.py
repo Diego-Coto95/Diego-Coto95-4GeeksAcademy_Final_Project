96a7a1c0ad2e2e675b89f1eb4fcead57137a68b4
Juan Diego Coto <jd506@hotmail.com>
2021-03-11 23:42:04 +0000

    Connecting the register Data Base with the front

--- src/api/models.py
+++ src/api/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(250), unique=False, nullable=False)
     comments = db.relationship("Comments", lazy=True)
 
     def __repr__(self):
--- src/front/js/component/register.js
+++ src/front/js/component/register.js
@@ -1,19 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Modal, Form, FormControl, Button } from "react-bootstrap";
+import { Redirect } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Register = () => {
 	const [show, setShow] = useState(false);
+	const { actions, store } = useContext(Context);
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [pass, setPass] = useState("");
+	const [redirect, setRedirect] = useState(false);
 
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
+
+	// //**********************************************SIGN UP**********************************************//
+	const handleSubmitRegister = e => {
+		e.preventDefault();
+		if (name === "" || email === "" || pass === "") {
+			alert("Nombre, correo y contraseña son requeridos");
+		} else {
+			console.log(name, email, pass);
+			actions.validateRegister(name, email, pass);
+			handleClose();
+		}
+	};
 
 	return (
 		<>
 			<Button variant="success" onClick={handleShow}>
 				Sign up
 			</Button>
-			<Modal show={show} onHide={handleClose}>
+			<Modal show={show} onHide={handleClose} onSubmit={e => handleSubmitRegister(e)}>
 				<Modal.Header closeButton>
 					<Modal.Title>
 						Sign in <br />
@@ -27,12 +46,20 @@ export const Register = () => {
 					<Form>
 						<Form.Group controlId="formBasicEmail" className="text-center">
 							<Form.Label> Name </Form.Label>
-							<Form.Control type="name" placeholder="Enter name" />
+							<Form.Control
+								type="name"
+								placeholder="Enter name"
+								onChange={e => setName(e.target.value)}
+							/>
 						</Form.Group>
 
 						<Form.Group controlId="formBasicEmail" className="text-center">
 							<Form.Label>Email address</Form.Label>
-							<Form.Control type="email" placeholder="someuser@gmail.com" />
+							<Form.Control
+								type="email"
+								placeholder="someuser@gmail.com"
+								onChange={e => setEmail(e.target.value)}
+							/>
 							<Form.Text className="text-muted">
 								We will never share your email with anyone else.
 							</Form.Text>
@@ -40,10 +67,14 @@ export const Register = () => {
 
 						<Form.Group controlId="formBasicPassword" className="text-center">
 							<Form.Label>Password</Form.Label>
-							<Form.Control type="password" placeholder="Password" />
+							<Form.Control
+								type="password"
+								placeholder="Password"
+								onChange={e => setPass(e.target.value)}
+							/>
 						</Form.Group>
 						<div className="text-center">
-							<Button variant="success" type="submit" onClick={handleClose}>
+							<Button variant="success" type="submit" onClick={e => handleSubmitRegister(e)}>
 								Sign in
 							</Button>
 						</div>
