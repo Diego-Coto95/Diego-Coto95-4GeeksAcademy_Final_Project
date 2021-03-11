120ba8f7d17233298344c32901c45906c9a88d3c
Juan Diego Coto <jd506@hotmail.com>
2021-03-11 20:49:22 +0000

    Check routes and adding views in admin

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,30 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+################################################### REGISTER ##########################################################
+@api.route('/register', methods=["POST"])
+def register():
+    if request.method == 'POST':
+        email = request.json.get("email", None)
+        password = request.json.get("password", None)
+
+        #Valida que se ingrese un nuevo email y contraseña
+        if not email:
+            return jsonify({"msg": "email is required"}), 400
+        if not password:
+            return jsonify({"msg": "Password is required"}), 400
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            return jsonify({"msg": "Username already exists"}), 400
+
+        user = User() # Crea un nuevo usuario 
+        user.email = email # email que ingresa el usuario
+        hashed_password = generate_password_hash(password) #Incripcion de la contraseña
+        print(password, hashed_password)
+        user.password = hashed_password # se le asigna al usuario el password que se acaba de generar
+        db.session.add(user) # agrga al usuario a la DB
+        db.session.commit()
+
+        return jsonify({"success": "Thanks. your register was successfully", "status": "true"}), 200
